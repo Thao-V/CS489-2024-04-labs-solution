931c23befb940fd4bfea4c6c830de70fff6cb2ae
thao.vu <thaovu@miu.edu>
2024-04-19 07:35:15 -0500

    removed a test

--- ads/ads-spring-boot/src/test/java/cs489/lab/ads/AdsApplicationTests.java
+++ ads/ads-spring-boot/src/test/java/cs489/lab/ads/AdsApplicationTests.java
@@ -3,7 +3,7 @@ package cs489.lab.ads;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AdsApplicationTests {
 
     @Test
